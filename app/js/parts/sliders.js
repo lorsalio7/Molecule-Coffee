fa966d9744b4471217786e25161521aaad28f657
lorsalio7 <lorsalio7@gmail.com>
2024-01-11 21:36:06 +0500

    Сверстать секцию Menu

--- app/js/parts/sliders.js
+++ app/js/parts/sliders.js
@@ -1,11 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
   let promoSlider = document.querySelector(".promo__slider");
+  let coffeeSlider = document.querySelector(".coffee-slider");
+  let snacksSlider = document.querySelector(".snacks-slider");
+  let seasonalMenuSlider = document.querySelector(".seasonal-menu-slider");
 
   if(promoSlider) {
     new Splide(promoSlider, {
       perMove: 1,
       arrows: true,
-      pagination: true
+      pagination: true,
+      rewind : true
+    }).mount();
+  }
+
+  if(coffeeSlider) {
+    new Splide(coffeeSlider, {
+      perPage: 5,
+      perMove: 1,
+      arrows: true,
+      pagination: false,
+      gap: 32
+    }).mount();
+  }
+
+  if(snacksSlider) {
+    new Splide(snacksSlider, {
+      perPage: 5,
+      perMove: 1,
+      arrows: true,
+      pagination: false,
+      gap: 32
+    }).mount();
+  }
+
+  if(seasonalMenuSlider) {
+    new Splide(seasonalMenuSlider, {
+      perPage: 5,
+      perMove: 1,
+      arrows: true,
+      pagination: false,
+      gap: 32
     }).mount();
   }
 });
--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,42 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   var promoSlider = document.querySelector(".promo__slider");
+  var coffeeSlider = document.querySelector(".coffee-slider");
+  var snacksSlider = document.querySelector(".snacks-slider");
+  var seasonalMenuSlider = document.querySelector(".seasonal-menu-slider");
   if (promoSlider) {
     new Splide(promoSlider, {
       perMove: 1,
       arrows: true,
-      pagination: true
+      pagination: true,
+      rewind: true
+    }).mount();
+  }
+  if (coffeeSlider) {
+    new Splide(coffeeSlider, {
+      perPage: 5,
+      perMove: 1,
+      arrows: true,
+      pagination: false,
+      gap: 32
+    }).mount();
+  }
+  if (snacksSlider) {
+    new Splide(snacksSlider, {
+      perPage: 5,
+      perMove: 1,
+      arrows: true,
+      pagination: false,
+      gap: 32
+    }).mount();
+  }
+  if (seasonalMenuSlider) {
+    new Splide(seasonalMenuSlider, {
+      perPage: 5,
+      perMove: 1,
+      arrows: true,
+      pagination: false,
+      gap: 32
     }).mount();
   }
 });
