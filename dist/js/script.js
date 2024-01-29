b34abe0a763d65e594d3231cf06610ded2f16284
lorsalio7 <lorsalio7@gmail.com>
2024-01-29 20:50:00 +0500

    Добавить отложенную загрузку для секции с картами

--- dist/js/script.js
+++ dist/js/script.js
@@ -182,171 +182,186 @@ if (mapsTabs) {
   });
 }
 ;
-var firstMapContainer = document.querySelector("#map-1");
-var secondMapContainer = document.querySelector("#map-2");
-var thirdMapContainer = document.querySelector("#map-3");
-ymaps.ready(function () {
-  var firstMapPlacemark = new ymaps.Placemark([53.91158508909563, 27.59594398101469], {}, {
-    iconLayout: "default#image",
-    iconImageHref: "img/map-pin.svg",
-    iconImageSize: [106, 107],
-    iconImageOffset: [-50, -80]
+var locationTabs = document.querySelector("#locations");
+if (locationTabs) {
+  var firstMapContainer = locationTabs.querySelector("#map-1");
+  var secondMapContainer = locationTabs.querySelector("#map-2");
+  var thirdMapContainer = locationTabs.querySelector("#map-3");
+  var mapLoaded = false;
+  var scrollTimeout;
+  var mapOffset = locationTabs.getBoundingClientRect();
+  window.addEventListener("scroll", function () {
+    clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(function () {
+      var scrollY = window.scrollY;
+      if (!mapLoaded && scrollY >= mapOffset.top - 500) {
+        ymaps.ready(function () {
+          var firstMapPlacemark = new ymaps.Placemark([53.91158508909563, 27.59594398101469], {}, {
+            iconLayout: "default#image",
+            iconImageHref: "img/map-pin.svg",
+            iconImageSize: [106, 107],
+            iconImageOffset: [-50, -80]
+          });
+          var firstMap = new ymaps.Map(firstMapContainer, {
+              center: [53.91158508909563, 27.59594398101469],
+              zoom: 17,
+              controls: [],
+              type: "yandex#map"
+            }),
+            ZoomLayout = ymaps.templateLayoutFactory.createClass("<div>" + "<div id='first-map-zoom-in' class='locations-tabs__zoom-button locations-tabs__zoom-button--zoom-in'>" + "<svg width='20' height='20' aria-hidden='true'>" + "<use xlink:href='img/sprite.svg#plus-ic'></use>" + "</svg>" + "</div>" + "<div id='first-map-zoom-out' class='locations-tabs__zoom-button locations-tabs__zoom-button--zoom-out'>" + "<svg width='20' height='20' aria-hidden='true'>" + "<use xlink:href='img/sprite.svg#minus-ic'></use>" + "</svg>" + "</div>" + "</div>", {
+              // Переопределяем методы макета, чтобы выполнять дополнительные действия
+              // при построении и очистке макета.
+              build: function build() {
+                ZoomLayout.superclass.build.call(this);
+                this.zoomInCallback = this.zoomIn.bind(this);
+                this.zoomOutCallback = this.zoomOut.bind(this);
+                document.getElementById("first-map-zoom-in").addEventListener('click', this.zoomInCallback);
+                document.getElementById("first-map-zoom-out").addEventListener('click', this.zoomOutCallback);
+              },
+              clear: function clear() {
+                document.getElementById("first-map-zoom-in").removeEventListener('click', this.zoomInCallback);
+                document.getElementById("first-map-zoom-out").removeEventListener('click', this.zoomOutCallback);
+                ZoomLayout.superclass.clear.call(this);
+              },
+              zoomIn: function zoomIn() {
+                var map = this.getData().control.getMap();
+                map.setZoom(map.getZoom() + 1, {
+                  checkZoomRange: true
+                });
+              },
+              zoomOut: function zoomOut() {
+                var map = this.getData().control.getMap();
+                map.setZoom(map.getZoom() - 1, {
+                  checkZoomRange: true
+                });
+              }
+            }),
+            zoomControl = new ymaps.control.ZoomControl({
+              options: {
+                layout: ZoomLayout,
+                position: {
+                  right: 25,
+                  bottom: 55
+                }
+              }
+            });
+          firstMap.geoObjects.add(firstMapPlacemark);
+          firstMap.behaviors.disable(["scrollZoom"]);
+          firstMap.controls.add(zoomControl);
+        });
+        mapLoaded = true;
+      }
+    }, 100);
   });
-  var firstMap = new ymaps.Map(firstMapContainer, {
-      center: [53.91158508909563, 27.59594398101469],
-      zoom: 17,
-      controls: [],
-      type: "yandex#map"
-    }),
-    ZoomLayout = ymaps.templateLayoutFactory.createClass("<div>" + "<div id='first-map-zoom-in' class='locations-tabs__zoom-button locations-tabs__zoom-button--zoom-in'>" + "<svg width='20' height='20' aria-hidden='true'>" + "<use xlink:href='img/sprite.svg#plus-ic'></use>" + "</svg>" + "</div>" + "<div id='first-map-zoom-out' class='locations-tabs__zoom-button locations-tabs__zoom-button--zoom-out'>" + "<svg width='20' height='20' aria-hidden='true'>" + "<use xlink:href='img/sprite.svg#minus-ic'></use>" + "</svg>" + "</div>" + "</div>", {
-      // Переопределяем методы макета, чтобы выполнять дополнительные действия
-      // при построении и очистке макета.
-      build: function build() {
-        ZoomLayout.superclass.build.call(this);
-        this.zoomInCallback = this.zoomIn.bind(this);
-        this.zoomOutCallback = this.zoomOut.bind(this);
-        document.getElementById("first-map-zoom-in").addEventListener('click', this.zoomInCallback);
-        document.getElementById("first-map-zoom-out").addEventListener('click', this.zoomOutCallback);
-      },
-      clear: function clear() {
-        document.getElementById("first-map-zoom-in").removeEventListener('click', this.zoomInCallback);
-        document.getElementById("first-map-zoom-out").removeEventListener('click', this.zoomOutCallback);
-        ZoomLayout.superclass.clear.call(this);
-      },
-      zoomIn: function zoomIn() {
-        var map = this.getData().control.getMap();
-        map.setZoom(map.getZoom() + 1, {
-          checkZoomRange: true
-        });
-      },
-      zoomOut: function zoomOut() {
-        var map = this.getData().control.getMap();
-        map.setZoom(map.getZoom() - 1, {
-          checkZoomRange: true
-        });
-      }
-    }),
-    zoomControl = new ymaps.control.ZoomControl({
-      options: {
-        layout: ZoomLayout,
-        position: {
-          right: 25,
-          bottom: 55
-        }
-      }
+  ymaps.ready(function () {
+    var secondMapPlacemark = new ymaps.Placemark([53.90560630126001, 27.532580720901436], {}, {
+      iconLayout: "default#image",
+      iconImageHref: "img/map-pin.svg",
+      iconImageSize: [106, 107],
+      iconImageOffset: [-30, -75]
     });
-  firstMap.geoObjects.add(firstMapPlacemark);
-  firstMap.behaviors.disable(["scrollZoom"]);
-  firstMap.controls.add(zoomControl);
-});
-ymaps.ready(function () {
-  var secondMapPlacemark = new ymaps.Placemark([53.90560630126001, 27.532580720901436], {}, {
-    iconLayout: "default#image",
-    iconImageHref: "img/map-pin.svg",
-    iconImageSize: [106, 107],
-    iconImageOffset: [-30, -75]
+    var secondMap = new ymaps.Map(secondMapContainer, {
+        center: [53.90560630126001, 27.532580720901436],
+        zoom: 17,
+        controls: [],
+        type: "yandex#map"
+      }),
+      ZoomLayout = ymaps.templateLayoutFactory.createClass("<div>" + "<div id='second-map-zoom-in' class='locations-tabs__zoom-button locations-tabs__zoom-button--zoom-in'>" + "<svg width='20' height='20' aria-hidden='true'>" + "<use xlink:href='img/sprite.svg#plus-ic'></use>" + "</svg>" + "</div>" + "<div id='second-map-zoom-out' class='locations-tabs__zoom-button locations-tabs__zoom-button--zoom-out'>" + "<svg width='20' height='20' aria-hidden='true'>" + "<use xlink:href='img/sprite.svg#minus-ic'></use>" + "</svg>" + "</div>" + "</div>", {
+        // Переопределяем методы макета, чтобы выполнять дополнительные действия
+        // при построении и очистке макета.
+        build: function build() {
+          ZoomLayout.superclass.build.call(this);
+          this.zoomInCallback = this.zoomIn.bind(this);
+          this.zoomOutCallback = this.zoomOut.bind(this);
+          document.getElementById("second-map-zoom-in").addEventListener('click', this.zoomInCallback);
+          document.getElementById("second-map-zoom-out").addEventListener('click', this.zoomOutCallback);
+        },
+        clear: function clear() {
+          document.getElementById("second-map-zoom-in").removeEventListener('click', this.zoomInCallback);
+          document.getElementById("second-map-zoom-out").removeEventListener('click', this.zoomOutCallback);
+          ZoomLayout.superclass.clear.call(this);
+        },
+        zoomIn: function zoomIn() {
+          var map = this.getData().control.getMap();
+          map.setZoom(map.getZoom() + 1, {
+            checkZoomRange: true
+          });
+        },
+        zoomOut: function zoomOut() {
+          var map = this.getData().control.getMap();
+          map.setZoom(map.getZoom() - 1, {
+            checkZoomRange: true
+          });
+        }
+      }),
+      zoomControl = new ymaps.control.ZoomControl({
+        options: {
+          layout: ZoomLayout,
+          position: {
+            right: 25,
+            bottom: 55
+          }
+        }
+      });
+    secondMap.geoObjects.add(secondMapPlacemark);
+    secondMap.behaviors.disable(["scrollZoom"]);
+    secondMap.controls.add(zoomControl);
   });
-  var secondMap = new ymaps.Map(secondMapContainer, {
-      center: [53.90560630126001, 27.532580720901436],
-      zoom: 17,
-      controls: [],
-      type: "yandex#map"
-    }),
-    ZoomLayout = ymaps.templateLayoutFactory.createClass("<div>" + "<div id='second-map-zoom-in' class='locations-tabs__zoom-button locations-tabs__zoom-button--zoom-in'>" + "<svg width='20' height='20' aria-hidden='true'>" + "<use xlink:href='img/sprite.svg#plus-ic'></use>" + "</svg>" + "</div>" + "<div id='second-map-zoom-out' class='locations-tabs__zoom-button locations-tabs__zoom-button--zoom-out'>" + "<svg width='20' height='20' aria-hidden='true'>" + "<use xlink:href='img/sprite.svg#minus-ic'></use>" + "</svg>" + "</div>" + "</div>", {
-      // Переопределяем методы макета, чтобы выполнять дополнительные действия
-      // при построении и очистке макета.
-      build: function build() {
-        ZoomLayout.superclass.build.call(this);
-        this.zoomInCallback = this.zoomIn.bind(this);
-        this.zoomOutCallback = this.zoomOut.bind(this);
-        document.getElementById("second-map-zoom-in").addEventListener('click', this.zoomInCallback);
-        document.getElementById("second-map-zoom-out").addEventListener('click', this.zoomOutCallback);
-      },
-      clear: function clear() {
-        document.getElementById("second-map-zoom-in").removeEventListener('click', this.zoomInCallback);
-        document.getElementById("second-map-zoom-out").removeEventListener('click', this.zoomOutCallback);
-        ZoomLayout.superclass.clear.call(this);
-      },
-      zoomIn: function zoomIn() {
-        var map = this.getData().control.getMap();
-        map.setZoom(map.getZoom() + 1, {
-          checkZoomRange: true
-        });
-      },
-      zoomOut: function zoomOut() {
-        var map = this.getData().control.getMap();
-        map.setZoom(map.getZoom() - 1, {
-          checkZoomRange: true
-        });
-      }
-    }),
-    zoomControl = new ymaps.control.ZoomControl({
-      options: {
-        layout: ZoomLayout,
-        position: {
-          right: 25,
-          bottom: 55
-        }
-      }
+  ymaps.ready(function () {
+    var thirdMapPlacemark = new ymaps.Placemark([53.89415735177822, 27.5500537901783], {}, {
+      iconLayout: "default#image",
+      iconImageHref: "img/map-pin.svg",
+      iconImageSize: [106, 107],
+      iconImageOffset: [-50, -90]
     });
-  secondMap.geoObjects.add(secondMapPlacemark);
-  secondMap.behaviors.disable(["scrollZoom"]);
-  secondMap.controls.add(zoomControl);
-});
-ymaps.ready(function () {
-  var thirdMapPlacemark = new ymaps.Placemark([53.89415735177822, 27.5500537901783], {}, {
-    iconLayout: "default#image",
-    iconImageHref: "img/map-pin.svg",
-    iconImageSize: [106, 107],
-    iconImageOffset: [-50, -90]
+    var thirdMap = new ymaps.Map(thirdMapContainer, {
+        center: [53.89415735177822, 27.5500537901783],
+        zoom: 17,
+        controls: [],
+        type: "yandex#map"
+      }),
+      ZoomLayout = ymaps.templateLayoutFactory.createClass("<div>" + "<div id='third-map-zoom-in' class='locations-tabs__zoom-button locations-tabs__zoom-button--zoom-in'>" + "<svg width='20' height='20' aria-hidden='true'>" + "<use xlink:href='img/sprite.svg#plus-ic'></use>" + "</svg>" + "</div>" + "<div id='third-map-zoom-out' class='locations-tabs__zoom-button locations-tabs__zoom-button--zoom-out'>" + "<svg width='20' height='20' aria-hidden='true'>" + "<use xlink:href='img/sprite.svg#minus-ic'></use>" + "</svg>" + "</div>" + "</div>", {
+        // Переопределяем методы макета, чтобы выполнять дополнительные действия
+        // при построении и очистке макета.
+        build: function build() {
+          ZoomLayout.superclass.build.call(this);
+          this.zoomInCallback = this.zoomIn.bind(this);
+          this.zoomOutCallback = this.zoomOut.bind(this);
+          document.getElementById("third-map-zoom-in").addEventListener('click', this.zoomInCallback);
+          document.getElementById("third-map-zoom-out").addEventListener('click', this.zoomOutCallback);
+        },
+        clear: function clear() {
+          document.getElementById("third-map-zoom-in").removeEventListener('click', this.zoomInCallback);
+          document.getElementById("third-map-zoom-out").removeEventListener('click', this.zoomOutCallback);
+          ZoomLayout.superclass.clear.call(this);
+        },
+        zoomIn: function zoomIn() {
+          var map = this.getData().control.getMap();
+          map.setZoom(map.getZoom() + 1, {
+            checkZoomRange: true
+          });
+        },
+        zoomOut: function zoomOut() {
+          var map = this.getData().control.getMap();
+          map.setZoom(map.getZoom() - 1, {
+            checkZoomRange: true
+          });
+        }
+      }),
+      zoomControl = new ymaps.control.ZoomControl({
+        options: {
+          layout: ZoomLayout,
+          position: {
+            right: 25,
+            bottom: 55
+          }
+        }
+      });
+    thirdMap.geoObjects.add(thirdMapPlacemark);
+    thirdMap.behaviors.disable(["scrollZoom"]);
+    thirdMap.controls.add(zoomControl);
   });
-  var thirdMap = new ymaps.Map(thirdMapContainer, {
-      center: [53.89415735177822, 27.5500537901783],
-      zoom: 17,
-      controls: [],
-      type: "yandex#map"
-    }),
-    ZoomLayout = ymaps.templateLayoutFactory.createClass("<div>" + "<div id='third-map-zoom-in' class='locations-tabs__zoom-button locations-tabs__zoom-button--zoom-in'>" + "<svg width='20' height='20' aria-hidden='true'>" + "<use xlink:href='img/sprite.svg#plus-ic'></use>" + "</svg>" + "</div>" + "<div id='third-map-zoom-out' class='locations-tabs__zoom-button locations-tabs__zoom-button--zoom-out'>" + "<svg width='20' height='20' aria-hidden='true'>" + "<use xlink:href='img/sprite.svg#minus-ic'></use>" + "</svg>" + "</div>" + "</div>", {
-      // Переопределяем методы макета, чтобы выполнять дополнительные действия
-      // при построении и очистке макета.
-      build: function build() {
-        ZoomLayout.superclass.build.call(this);
-        this.zoomInCallback = this.zoomIn.bind(this);
-        this.zoomOutCallback = this.zoomOut.bind(this);
-        document.getElementById("third-map-zoom-in").addEventListener('click', this.zoomInCallback);
-        document.getElementById("third-map-zoom-out").addEventListener('click', this.zoomOutCallback);
-      },
-      clear: function clear() {
-        document.getElementById("third-map-zoom-in").removeEventListener('click', this.zoomInCallback);
-        document.getElementById("third-map-zoom-out").removeEventListener('click', this.zoomOutCallback);
-        ZoomLayout.superclass.clear.call(this);
-      },
-      zoomIn: function zoomIn() {
-        var map = this.getData().control.getMap();
-        map.setZoom(map.getZoom() + 1, {
-          checkZoomRange: true
-        });
-      },
-      zoomOut: function zoomOut() {
-        var map = this.getData().control.getMap();
-        map.setZoom(map.getZoom() - 1, {
-          checkZoomRange: true
-        });
-      }
-    }),
-    zoomControl = new ymaps.control.ZoomControl({
-      options: {
-        layout: ZoomLayout,
-        position: {
-          right: 25,
-          bottom: 55
-        }
-      }
-    });
-  thirdMap.geoObjects.add(thirdMapPlacemark);
-  thirdMap.behaviors.disable(["scrollZoom"]);
-  thirdMap.controls.add(zoomControl);
-});
+}
 ;
 var phoneInput = document.querySelector(".order-form__phone");
 var phoneCode = document.querySelector(".order-form__operator-code");
